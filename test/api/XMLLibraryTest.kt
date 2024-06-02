040f43bd933b79fa1e38cbb769b3217639438228
adrianalf98 <118607738+adrianalf98@users.noreply.github.com>
2024-06-03 00:55:31 +0100

    Testes adicionais

--- test/api/XMLLibraryTest.kt
+++ test/api/XMLLibraryTest.kt
@@ -91,7 +91,6 @@ internal class XMLLibraryTest {
         val fuc = FUC("M4310", "Programação Avançada", 6.0,
             "la la...", listOf(componenteAvaliacao1, componenteAvaliacao2))
         val entidade = lib.criarEntidade(fuc)
-        println(lib.prettyPrint(entidade))
         assertEquals(lib.prettyPrint(entidade), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
                 "<FUC codigo=\"M4310\">\n" +
                 "\t<ects>6.0</ects>\n" +
@@ -291,6 +290,17 @@ internal class XMLLibraryTest {
         assertEquals(resultado, esperado)
     }
 
+    @Test
+    fun testeGerarXmlWithInvalidDataClass() {
+        class NonDataClass(val attribute: String)
+
+        val obj = NonDataClass(attribute = "value")
+
+        assertThrows<IllegalArgumentException> {
+            lib.gerarXML(obj)
+        }
+    }
+
     /*
     *Testa se a biblioteca consegue adicionar atributos globalmente
     * a todas as entidades de um determinado tipo.
@@ -347,6 +357,58 @@ internal class XMLLibraryTest {
         assertEquals(resultado3, esperado3)
     }
 
+    @Test
+    fun testeAdiconarAtributoGlobalmenteVazio(){
+        val componenteAvaliacao1fuc1 = ComponenteAvaliacao("Quizzes", 20)
+        val componenteAvaliacao2fuc1 = ComponenteAvaliacao("Projeto", 80)
+        val fuc1 = FUC("M4310", "Programação Avançada", 6.0,
+            "la la...", listOf(componenteAvaliacao1fuc1, componenteAvaliacao2fuc1))
+
+        val componenteAvaliacao1fuc2 = ComponenteAvaliacao("Dissertação", 60)
+        val componenteAvaliacao2fuc2 = ComponenteAvaliacao("Apresentação", 20)
+        val componenteAvaliacao3fuc2 = ComponenteAvaliacao("Discussão", 20)
+        val fuc2 = FUC("03782", "Dissertação", 42.0,
+            "la la...", listOf(componenteAvaliacao1fuc2, componenteAvaliacao2fuc2, componenteAvaliacao3fuc2))
+
+        val plano = Plano(listOf(fuc1, fuc2))
+        val entidade = lib.criarEntidade(plano)
+
+        lib.adicionarAtributoGlobalmente(entidade, "", "valorMaximo", "20")
+
+        val resultado1 = lib.prettyPrint(entidade)
+        val esperado1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<plano>\n" +
+                "\t<FUC codigo=\"M4310\">\n" +
+                "\t\t<ects>6.0</ects>\n" +
+                "\t\t<nome>Programação Avançada</nome>\n" +
+                "\t\t<componente nome=\"Quizzes\" peso=\"20%\"/>\n" +
+                "\t\t<componente nome=\"Projeto\" peso=\"80%\"/>\n" +
+                "\t</FUC>\n" +
+                "\t<FUC codigo=\"03782\">\n" +
+                "\t\t<ects>42.0</ects>\n" +
+                "\t\t<nome>Dissertação</nome>\n" +
+                "\t\t<componente nome=\"Dissertação\" peso=\"60%\"/>\n" +
+                "\t\t<componente nome=\"Apresentação\" peso=\"20%\"/>\n" +
+                "\t\t<componente nome=\"Discussão\" peso=\"20%\"/>\n" +
+                "\t</FUC>\n" +
+                "</plano>"
+
+        assertEquals(resultado1, esperado1)
+
+        lib.adicionarAtributoGlobalmente(entidade, "componente", "", "20")
+
+        val resultado2 = lib.prettyPrint(entidade)
+        val esperado2 = esperado1
+
+        assertEquals(resultado2, esperado2)
+
+        lib.adicionarAtributoGlobalmente(entidade, "componente", "valorMaximo", "")
+
+        val resultado3 = lib.prettyPrint(entidade)
+        val esperado3 = esperado1
+        assertEquals(resultado3, esperado3)
+    }
+
     /*
     * Testa se a biblioteca pode renomear entidades globalmente.
      */
@@ -393,9 +455,52 @@ internal class XMLLibraryTest {
         assertEquals(resultado2, esperado)
     }
 
+    @Test
+    fun testeRenomearEntidadesGlobalmenteVazio(){
+        val componenteAvaliacao1fuc1 = ComponenteAvaliacao("Quizzes", 20)
+        val componenteAvaliacao2fuc1 = ComponenteAvaliacao("Projeto", 80)
+        val fuc1 = FUC("M4310", "Programação Avançada", 6.0,
+            "la la...", listOf(componenteAvaliacao1fuc1, componenteAvaliacao2fuc1))
+
+        val componenteAvaliacao1fuc2 = ComponenteAvaliacao("Dissertação", 60)
+        val componenteAvaliacao2fuc2 = ComponenteAvaliacao("Apresentação", 20)
+        val componenteAvaliacao3fuc2 = ComponenteAvaliacao("Discussão", 20)
+        val fuc2 = FUC("03782", "Dissertação", 42.0,
+            "la la...", listOf(componenteAvaliacao1fuc2, componenteAvaliacao2fuc2, componenteAvaliacao3fuc2))
+
+        val esperado = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<plano>\n" +
+                "\t<FUC codigo=\"M4310\">\n" +
+                "\t\t<ects>6.0</ects>\n" +
+                "\t\t<nome>Programação Avançada</nome>\n" +
+                "\t\t<componente nome=\"Quizzes\" peso=\"20%\"/>\n" +
+                "\t\t<componente nome=\"Projeto\" peso=\"80%\"/>\n" +
+                "\t</FUC>\n" +
+                "\t<FUC codigo=\"03782\">\n" +
+                "\t\t<ects>42.0</ects>\n" +
+                "\t\t<nome>Dissertação</nome>\n" +
+                "\t\t<componente nome=\"Dissertação\" peso=\"60%\"/>\n" +
+                "\t\t<componente nome=\"Apresentação\" peso=\"20%\"/>\n" +
+                "\t\t<componente nome=\"Discussão\" peso=\"20%\"/>\n" +
+                "\t</FUC>\n" +
+                "</plano>"
+
+        val plano = Plano(listOf(fuc1, fuc2))
+        val entidade = lib.criarEntidade(plano)
+
+        val resultado1 = lib.prettyPrint(entidade)
+        assertEquals(resultado1, esperado)
+        lib.renomearEntidadesGlobalmente(entidade, "", "componenteAvaliacao")
+
+        val resultado2 = lib.prettyPrint(entidade)
+
+
+        assertEquals(resultado2, esperado)
+    }
+
     /*
     * Testa se a biblioteca pode remover entidades globalmente.
-     */
+    */
     @Test
     fun testeRemoverEntidadesGlobalmente(){
         val componenteAvaliacao1fuc1 = ComponenteAvaliacao("Quizzes", 20)
@@ -428,6 +533,50 @@ internal class XMLLibraryTest {
         assertNotEquals(resultado1, esperado)
 
         lib.removerEntidadesGlobalmente(entidade, "componente")
+
+        val resultado2 = lib.prettyPrint(entidade)
+
+
+        assertEquals(resultado2, esperado)
+    }
+
+    @Test
+    fun testeRemoverEntidadesGlobalmenteVazio(){
+        val componenteAvaliacao1fuc1 = ComponenteAvaliacao("Quizzes", 20)
+        val componenteAvaliacao2fuc1 = ComponenteAvaliacao("Projeto", 80)
+        val fuc1 = FUC("M4310", "Programação Avançada", 6.0,
+            "la la...", listOf(componenteAvaliacao1fuc1, componenteAvaliacao2fuc1))
+
+        val componenteAvaliacao1fuc2 = ComponenteAvaliacao("Dissertação", 60)
+        val componenteAvaliacao2fuc2 = ComponenteAvaliacao("Apresentação", 20)
+        val componenteAvaliacao3fuc2 = ComponenteAvaliacao("Discussão", 20)
+        val fuc2 = FUC("03782", "Dissertação", 42.0,
+            "la la...", listOf(componenteAvaliacao1fuc2, componenteAvaliacao2fuc2, componenteAvaliacao3fuc2))
+
+        val esperado = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                        "<plano>\n" +
+                        "\t<FUC codigo=\"M4310\">\n" +
+                        "\t\t<ects>6.0</ects>\n" +
+                        "\t\t<nome>Programação Avançada</nome>\n" +
+                        "\t\t<componente nome=\"Quizzes\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Projeto\" peso=\"80%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "\t<FUC codigo=\"03782\">\n" +
+                        "\t\t<ects>42.0</ects>\n" +
+                        "\t\t<nome>Dissertação</nome>\n" +
+                        "\t\t<componente nome=\"Dissertação\" peso=\"60%\"/>\n" +
+                        "\t\t<componente nome=\"Apresentação\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Discussão\" peso=\"20%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "</plano>"
+
+        val plano = Plano(listOf(fuc1, fuc2))
+        val entidade = lib.criarEntidade(plano)
+
+        val resultado1 = lib.prettyPrint(entidade)
+        assertEquals(resultado1, esperado)
+
+        lib.removerEntidadesGlobalmente(entidade, "")
 
         val resultado2 = lib.prettyPrint(entidade)
 
@@ -480,8 +629,50 @@ internal class XMLLibraryTest {
         assertEquals(resultado2, esperado)
     }
 
-    /*
-    * Testa se a biblioteca consegue remover atributos globalmente.
+    @Test
+    fun testeRenomearAtributosGlobalmenteVazio(){
+        val componenteAvaliacao1fuc1 = ComponenteAvaliacao("Quizzes", 20)
+        val componenteAvaliacao2fuc1 = ComponenteAvaliacao("Projeto", 80)
+        val fuc1 = FUC("M4310", "Programação Avançada", 6.0,
+            "la la...", listOf(componenteAvaliacao1fuc1, componenteAvaliacao2fuc1))
+
+        val componenteAvaliacao1fuc2 = ComponenteAvaliacao("Dissertação", 60)
+        val componenteAvaliacao2fuc2 = ComponenteAvaliacao("Apresentação", 20)
+        val componenteAvaliacao3fuc2 = ComponenteAvaliacao("Discussão", 20)
+        val fuc2 = FUC("03782", "Dissertação", 42.0,
+            "la la...", listOf(componenteAvaliacao1fuc2, componenteAvaliacao2fuc2, componenteAvaliacao3fuc2))
+
+        val esperado = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                        "<plano>\n" +
+                        "\t<FUC codigo=\"M4310\">\n" +
+                        "\t\t<ects>6.0</ects>\n" +
+                        "\t\t<nome>Programação Avançada</nome>\n" +
+                        "\t\t<componente nome=\"Quizzes\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Projeto\" peso=\"80%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "\t<FUC codigo=\"03782\">\n" +
+                        "\t\t<ects>42.0</ects>\n" +
+                        "\t\t<nome>Dissertação</nome>\n" +
+                        "\t\t<componente nome=\"Dissertação\" peso=\"60%\"/>\n" +
+                        "\t\t<componente nome=\"Apresentação\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Discussão\" peso=\"20%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "</plano>"
+
+        val plano = Plano(listOf(fuc1, fuc2))
+        val entidade = lib.criarEntidade(plano)
+
+        val resultado1 = lib.prettyPrint(entidade)
+        assertEquals(resultado1, esperado)
+
+        lib.renomearAtributosGlobalmente(entidade, "", "identificador")
+
+        val resultado2 = lib.prettyPrint(entidade)
+        assertEquals(resultado2, esperado)
+    }
+    
+    /**
+     * Testa se a biblioteca consegue remover atributos globalmente.
      */
     @Test
     fun testeRemoverAtributosGlobalmente(){
@@ -520,6 +711,48 @@ internal class XMLLibraryTest {
         assertNotEquals(resultado1, esperado)
 
         lib.removerAtributosGlobalmente(entidade, "componente", "peso")
+
+        val resultado2 = lib.prettyPrint(entidade)
+        assertEquals(resultado2, esperado)
+    }
+
+    @Test
+    fun testeRemoverAtributosGlobalmenteVazio(){
+        val componenteAvaliacao1fuc1 = ComponenteAvaliacao("Quizzes", 20)
+        val componenteAvaliacao2fuc1 = ComponenteAvaliacao("Projeto", 80)
+        val fuc1 = FUC("M4310", "Programação Avançada", 6.0,
+            "la la...", listOf(componenteAvaliacao1fuc1, componenteAvaliacao2fuc1))
+
+        val componenteAvaliacao1fuc2 = ComponenteAvaliacao("Dissertação", 60)
+        val componenteAvaliacao2fuc2 = ComponenteAvaliacao("Apresentação", 20)
+        val componenteAvaliacao3fuc2 = ComponenteAvaliacao("Discussão", 20)
+        val fuc2 = FUC("03782", "Dissertação", 42.0,
+            "la la...", listOf(componenteAvaliacao1fuc2, componenteAvaliacao2fuc2, componenteAvaliacao3fuc2))
+
+        val esperado = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                        "<plano>\n" +
+                        "\t<FUC codigo=\"M4310\">\n" +
+                        "\t\t<ects>6.0</ects>\n" +
+                        "\t\t<nome>Programação Avançada</nome>\n" +
+                        "\t\t<componente nome=\"Quizzes\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Projeto\" peso=\"80%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "\t<FUC codigo=\"03782\">\n" +
+                        "\t\t<ects>42.0</ects>\n" +
+                        "\t\t<nome>Dissertação</nome>\n" +
+                        "\t\t<componente nome=\"Dissertação\" peso=\"60%\"/>\n" +
+                        "\t\t<componente nome=\"Apresentação\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Discussão\" peso=\"20%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "</plano>"
+
+        val plano = Plano(listOf(fuc1, fuc2))
+        val entidade = lib.criarEntidade(plano)
+
+        val resultado1 = lib.prettyPrint(entidade)
+        assertEquals(resultado1, esperado)
+
+        lib.removerAtributosGlobalmente(entidade, "", "peso")
 
         val resultado2 = lib.prettyPrint(entidade)
         assertEquals(resultado2, esperado)
@@ -570,6 +803,48 @@ internal class XMLLibraryTest {
         assertEquals(resultado2, esperado)
     }
 
+    @Test
+    fun testeAlterarAtributosGlobalmenteVazio(){
+        val componenteAvaliacao1fuc1 = ComponenteAvaliacao("Quizzes", 20)
+        val componenteAvaliacao2fuc1 = ComponenteAvaliacao("Projeto", 80)
+        val fuc1 = FUC("M4310", "Programação Avançada", 6.0,
+            "la la...", listOf(componenteAvaliacao1fuc1, componenteAvaliacao2fuc1))
+
+        val componenteAvaliacao1fuc2 = ComponenteAvaliacao("Dissertação", 60)
+        val componenteAvaliacao2fuc2 = ComponenteAvaliacao("Apresentação", 20)
+        val componenteAvaliacao3fuc2 = ComponenteAvaliacao("Discussão", 20)
+        val fuc2 = FUC("03782", "Dissertação", 42.0,
+            "la la...", listOf(componenteAvaliacao1fuc2, componenteAvaliacao2fuc2, componenteAvaliacao3fuc2))
+
+        val esperado = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                        "<plano>\n" +
+                        "\t<FUC codigo=\"M4310\">\n" +
+                        "\t\t<ects>6.0</ects>\n" +
+                        "\t\t<nome>Programação Avançada</nome>\n" +
+                        "\t\t<componente nome=\"Quizzes\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Projeto\" peso=\"80%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "\t<FUC codigo=\"03782\">\n" +
+                        "\t\t<ects>42.0</ects>\n" +
+                        "\t\t<nome>Dissertação</nome>\n" +
+                        "\t\t<componente nome=\"Dissertação\" peso=\"60%\"/>\n" +
+                        "\t\t<componente nome=\"Apresentação\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Discussão\" peso=\"20%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "</plano>"
+
+        val plano = Plano(listOf(fuc1, fuc2))
+        val entidade = lib.criarEntidade(plano)
+
+        val resultado1 = lib.prettyPrint(entidade)
+        assertEquals(resultado1, esperado)
+
+        lib.alterarAtributosGlobalmente(entidade, "componente", "", "10")
+
+        val resultado2 = lib.prettyPrint(entidade)
+        assertEquals(resultado2, esperado)
+    }
+
     /*
     * Testa se a biblioteca consegue adicionar sub-entidades
     * a uma entidade existente.
@@ -613,6 +888,48 @@ internal class XMLLibraryTest {
         assertNotEquals(resultado1, esperado)
 
         lib.adicionarSubEntidade(entidade, "regente","FUC")
+
+        val resultado2 = lib.prettyPrint(entidade)
+        assertEquals(resultado2, esperado)
+    }
+
+    @Test
+    fun testeAdicionarSubEntidadeVazio(){
+        val componenteAvaliacao1fuc1 = ComponenteAvaliacao("Quizzes", 20)
+        val componenteAvaliacao2fuc1 = ComponenteAvaliacao("Projeto", 80)
+        val fuc1 = FUC("M4310", "Programação Avançada", 6.0,
+            "la la...", listOf(componenteAvaliacao1fuc1, componenteAvaliacao2fuc1))
+
+        val componenteAvaliacao1fuc2 = ComponenteAvaliacao("Dissertação", 60)
+        val componenteAvaliacao2fuc2 = ComponenteAvaliacao("Apresentação", 20)
+        val componenteAvaliacao3fuc2 = ComponenteAvaliacao("Discussão", 20)
+        val fuc2 = FUC("03782", "Dissertação", 42.0,
+            "la la...", listOf(componenteAvaliacao1fuc2, componenteAvaliacao2fuc2, componenteAvaliacao3fuc2))
+
+        val esperado = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                        "<plano>\n" +
+                        "\t<FUC codigo=\"M4310\">\n" +
+                        "\t\t<ects>6.0</ects>\n" +
+                        "\t\t<nome>Programação Avançada</nome>\n" +
+                        "\t\t<componente nome=\"Quizzes\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Projeto\" peso=\"80%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "\t<FUC codigo=\"03782\">\n" +
+                        "\t\t<ects>42.0</ects>\n" +
+                        "\t\t<nome>Dissertação</nome>\n" +
+                        "\t\t<componente nome=\"Dissertação\" peso=\"60%\"/>\n" +
+                        "\t\t<componente nome=\"Apresentação\" peso=\"20%\"/>\n" +
+                        "\t\t<componente nome=\"Discussão\" peso=\"20%\"/>\n" +
+                        "\t</FUC>\n" +
+                        "</plano>"
+
+        val plano = Plano(listOf(fuc1, fuc2))
+        val entidade = lib.criarEntidade(plano)
+
+        val resultado1 = lib.prettyPrint(entidade)
+        assertEquals(resultado1, esperado)
+
+        lib.adicionarSubEntidade(entidade, "","FUC")
 
         val resultado2 = lib.prettyPrint(entidade)
         assertEquals(resultado2, esperado)
